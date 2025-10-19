89eff38d7a9e400a3b219240af4d0ddb943d1f34
TheShahOne <116767447+TheShahOne@users.noreply.github.com>
2025-10-19 12:11:30 -0600

    Initial commit

---
+++ newfile.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Assignment 1</title>
+  </head>
+  <body>
+    <h1>Assignment 1 First Commit</h1>
+    <p>
+      <?php echo "xampp test"; ?>
+    </p>
+  </body>
+</html>
