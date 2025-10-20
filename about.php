fab1811807f1415528aa53ec6fe7f23608813c74
TheShahOne <116767447+TheShahOne@users.noreply.github.com>
2025-10-19 22:32:02 -0600

    about page and styling

--- about.php
+++ about.php
@@ -19,5 +19,28 @@
     </div>
   </nav>
 
+  <main class="about-page">
+    <section class="about-content">
+      <h2>About This Project</h2>
+
+      <p><strong>Author:</strong> Sadman Shahryier</p>
+      <p><strong>Course:</strong> COMP3512 – Web II, Mount Royal University</p>
+      <p><strong>Assignment:</strong> Assignment #1 – Portfolio Prowl</p>
+
+      <p><strong>Description:</strong>  
+      This assignment is a stock portfolio web app built with PHP and SQLite.  
+      It displays users, their investments, and company details, and includes API routes 
+      that return the required data in JSON format for future use.</p>
+
+
+      <p><strong>Technologies Used:</strong> HTML, CSS, PHP, and SQLite Database</p>
+
+      <p><strong>Repository:</strong>  
+        <a href="https://github.com/TheShahOne/WEB2-COMP3512-A1-2025" target="_blank">
+          github.com/TheShahOne/WEB2-COMP3512-A1-2025
+        </a>
+      </p>
+    </section>
+  </main>
 </body>
 </html>
